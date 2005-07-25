b478b9dd03793e329d81e5b930d62f16fd70c750
Guillem Jover <guillem@hadrons.org>
2005-07-25 00:07:29 +0000

    fgetln: Cleanup function
    Reindent, remove commented code and translate variable names to english

--- fgetln.c
+++ fgetln.c
@@ -9,24 +9,20 @@ char *
 fgetln (stream, len)
 	FILE *stream;
 	size_t *len;
-
 {
 	char *line=NULL;
-	size_t leido = 0;
-	
-	while (leido == 1) {
-	if ((leido = getline (&line, len, stream)) == -1)
-		return NULL;
+	size_t nread = 0;
+
+	while (nread == 1) {
+		nread = getline (&line, len, stream);
+		if (nread == -1)
+			return NULL;
 	}
 
-//	if (*(line+leido) != '\n')
-//	if (leido != 0)
-		(*len)--; /* get rid of the trailing \0, fgetln
-			     does not have it */
-//	if (leido == 1)
-//		leido = getline (&line, len, stream);
+	(*len)--; /* get rid of the trailing \0, fgetln
+		     does not have it */
 
-//	printf ("Caracter '%c' - Leido '%d'\n", *(line+leido-1), leido);
 	return line;
 }
 #endif
+
