67e48af044abf058167a60b79d4165f0431fac5a
Guillem Jover <guillem@hadrons.org>
2006-02-14 07:01:21 +0000

    Remove unused includes and protect header

--- include/bsd/getopt.h
+++ include/bsd/getopt.h
@@ -1,4 +1,10 @@
+#ifndef LIBBSD_GETOPT_H
+#define LIBBSD_GETOPT_H
+
 #include <getopt.h>
+
 extern int optreset;
 
 int bsd_getopt (int, char **, char *);
+
+#endif
--- include/bsd/stdlib.h
+++ include/bsd/stdlib.h
@@ -1,9 +1,6 @@
 #ifndef LIBBSD_STDLIB_H
 #define LIBBSD_STDLIB_H
 
-#include <sys/types.h>
-#include <stdio.h>
-
 const char *fmtcheck(const char *, const char *);
 
 #endif
